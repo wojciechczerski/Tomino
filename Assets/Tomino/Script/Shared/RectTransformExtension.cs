7a2f2dd92e95f983c38125612867f0946a180035
Wojciech Czerski <wojciech.czerski@gmail.com>
2024-01-12 02:08:50 +0100

    Add and use the SafeArea script

---
+++ Assets/Tomino/Script/Shared/RectTransformExtension.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public static class RectTransformExtension
+{
+    public static void SetLeft(this RectTransform rt, float left)
+    {
+        rt.offsetMin = new Vector2(left, rt.offsetMin.y);
+    }
+
+    public static void SetRight(this RectTransform rt, float right)
+    {
+        rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
+    }
+
+    public static void SetTop(this RectTransform rt, float top)
+    {
+        rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
+    }
+
+    public static void SetBottom(this RectTransform rt, float bottom)
+    {
+        rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
+    }
+}
---
+++ Assets/Tomino/Script/Shared/SafeArea.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Dazzle
+{
+    public class SafeArea : MonoBehaviour
+    {
+        public Camera mainCamera;
+
+        private RectTransform _rectTransform;
+        private RectTransform _parentTransform;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _parentTransform = _rectTransform.parent as RectTransform;
+        }
+
+        private void Update()
+        {
+            var safeArea = Screen.safeArea;
+            var safeAreaBottomLeft = new Vector2(safeArea.xMin, safeArea.yMin);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _parentTransform,
+                safeAreaBottomLeft,
+                mainCamera,
+                out var localBottomLeft
+            );
+
+            var safeAreaTopRight = new Vector2(safeArea.xMax, safeArea.yMax);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _parentTransform,
+                safeAreaTopRight,
+                mainCamera,
+                out var localTopRight
+            );
+
+            var rect = _parentTransform.rect;
+            _rectTransform.SetLeft(localBottomLeft.x + rect.width / 2);
+            _rectTransform.SetRight(rect.width / 2 - localTopRight.x);
+            _rectTransform.SetBottom(localBottomLeft.y + rect.height / 2);
+            _rectTransform.SetTop(rect.height / 2 - localTopRight.y);
+        }
+    }
+}
